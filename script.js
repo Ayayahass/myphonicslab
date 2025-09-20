dedcd9e9c4342ab0f8a42b7faddfba779ea9f5a1
Ayayahass <imi0t0200@gmail.com>
2025-09-20 10:42:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function playSound(letter) {
-  const audio = new Audio(`sounds/${letter}.mp3`);
-  audio.play();
+function playSound(letter, isCapital = false) {
+  // إذا كان الحرف كبير استخدم الملف الكبير
+  const fileName = isCapital ? `${letter.toUpperCase()}.mp3` : `${letter.toLowerCase()}.mp3`;
+  
+  const audio = new Audio(`sounds/${fileName}`);
+  audio.play().catch(() => {
+    alert(`الصوت غير موجود: sounds/${fileName}`);
+  });
 }
