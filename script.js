13098cd4b9d8b98ae0f3de0bef84283275997588
Ayayahass <imi0t0200@gmail.com>
2025-09-20 13:18:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function playSound(letter, isCapital = false) {
-  // إذا كان الحرف كبير استخدم الملف الكبير
-  const fileName = isCapital ? `${letter.toUpperCase()}.mp3` : `${letter.toLowerCase()}.mp3`;
-  
-  const audio = new Audio(`sounds/${fileName}`);
-  audio.play().catch(() => {
-    alert(`الصوت غير موجود: sounds/${fileName}`);
-  });
-}
